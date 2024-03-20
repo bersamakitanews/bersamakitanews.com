1a98b34231f4727174158f0e44523630764164ef
bersamakitanews <159485631+bersamakitanews@users.noreply.github.com>
2024-03-20 21:19:33 +0700

    Create CounterSuccess.js

---
+++ CounterSuccess.js
@@ -0,0 +1,8 @@
+function numberWithCommas(x) {return x.toString().replace(/B(?=(d{3})+(?!d))/g, ",");}
+
+function successHandler(msg) {let numberFormatted = numberWithCommas(msg.count);
+$('#counter-success').html(numberFormatted + " sedang menonton");}
+
+function errorHandler(msg) {console.log('conterview status:'.msg.statusText);return;}
+
+var defaultConfig = {channel:'detikcom-breakingnews',successCallback:successHandler,errorCallback:errorHandler,requestWinPostMsg:true};bersamakitanewsLiveUserCounter.setConfig(defaultConfig);
