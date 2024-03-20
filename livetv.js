c867ff7c008b9d7a5050adf34d40e822581242ee
bersamakitanews <159485631+bersamakitanews@users.noreply.github.com>
2024-03-20 20:40:26 +0700

    Update livetv.js

--- livetv.js
+++ livetv.js
@@ -1,6 +1,6 @@
 const detikvideo_props= {"target":"#main_live_chat","autoplay":false,"mute":false,"live":true,
                          
-"imageUrl": "https://akcdn.detik.net.id/community/media/visual/2024/03/13/detik-sore-1.png?w=800",
+"imageUrl": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEimpHAwtyA_ar8S47-WBtEeRZjXSwc3wz8QygVlKQd2KnBVy6digarhQyBPKJHxOjRC3o2sE_HlvDjG_3yY5zghFwWhqUVeqEzm1oofoP1ckMmY10haRjT06p6U1xNHhkJs6LKHbj4M8Lk6vFBELeikJUZ2ottEnyeKCMSBK7r0reWSWLa5ikfIw8FkCPX3/s16000/Template%20Tv%20Online.jpg",
    
 "videoUrl": "https://078527f52820.entrypoint.cloud.wowza.com/app-72g1m10R/ngrp:2732239f_all/playlist.m3u8",
    
