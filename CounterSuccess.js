2fcfb365c991624252ae7e529e755e9fa2d320e8
bersamakitanews <159485631+bersamakitanews@users.noreply.github.com>
2025-01-21 01:25:21 +0700

    Update CounterSuccess.js

--- CounterSuccess.js
+++ CounterSuccess.js
@@ -1,8 +1,22 @@
-function numberWithCommas(x) {return x.toString().replace(/B(?=(d{3})+(?!d))/g, ",");}
+function numberWithCommas(x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }
 
-function successHandler(msg) {let numberFormatted = numberWithCommas(msg.count);
-$('#counter-success').html(numberFormatted + " sedang menonton");}
+        function successHandler(msg) {
+            let numberFormatted = numberWithCommas(msg.count);
+            $('#counter-success').html(numberFormatted + " sedang menonton");
+        }
 
-function errorHandler(msg) {console.log('conterview status:'.msg.statusText);return;}
+        function errorHandler(msg) {
+            console.log('conterview status :'.msg.statusText);
+          return;
+        }
 
-var defaultConfig = {channel:'detikcom-breakingnews',successCallback:successHandler,errorCallback:errorHandler,requestWinPostMsg:true};bersamakitanewsLiveUserCounter.setConfig(defaultConfig);
+        var defaultConfig = {
+            channel: 'detikcom-breakingnews',
+            successCallback: successHandler,
+            errorCallback: errorHandler,
+            requestWinPostMsg: true
+        };
+
+        bersamakitanewsLiveUserCounter.setConfig(defaultConfig);
